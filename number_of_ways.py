c04caf5569eab76ccc81f23153946e2aab57fa61
xinmengZ <xinmeng.zhang@vanderbilt.edu>
2024-10-28 12:33:22 -0500

    fix error

--- number_of_ways.py
+++ number_of_ways.py
@@ -1,37 +1,37 @@
 from tqdm import tqdm
 
 
-def numberOfWays(startPos: int, endPos: int, k: int) -> int:
+def number_of_ways(start_pos: int, end_pos: int, k: int) -> int:
     """
     Solving Leetcode Problem.
     https://leetcode.com/problems/number-of-ways-to-reach-a-position-after-exactly-k-steps/
-    
+
     Given two positive integers startPos and endPos
     Initially, you are standing at position startPos on an infinite
     number line. With one step, you can move either one position to the left,
     or one position to the right.
-    
+
     Given a positive integer k, return the number of different ways to
     reach the position endPos starting from startPos, such that you
     perform exactly k steps.
     """
     # start with path of length 1
-    paths = [[startPos]]
+    paths = [[start_pos]]
 
     # loop k times
     for i in tqdm(range(k)):
-        for path in paths:
-            new_path = path.copy()
+        for _ in range(len(paths)):
+            new_path = paths.pop(0)
             last_position = new_path[-1]
 
             # exist fast if not going to make to end
-            if endPos - last_position > (k - i - 1):
+            if end_pos - last_position > (k - i):
                 continue
             # path that goes to the left
             new_path_left = new_path + [last_position - 1]
 
             # path that goes to the right
-            new_path_right = new_path + [last_position - 1]
+            new_path_right = new_path + [last_position + 1]
 
             # add paths to the left and right
             paths.append(new_path_left)
@@ -39,8 +39,8 @@ def numberOfWays(startPos: int, endPos: int, k: int) -> int:
 
     num_ways = 0
     for path in paths:
-        if path[-1] == endPos:
-            new_ways += 1
+        if path[-1] == end_pos:
+            num_ways += 1
     return num_ways
 
 
@@ -57,7 +57,8 @@ def test_number_of_ways():
     - 1 -> 0 -> 1 -> 2.
     It can be proven that no other way is possible, so we return 3.
     """
-    print(numberOfWays(1, 2, 3))
+    print(number_of_ways(1, 2, 3))
+
 
 if __name__ == "__main__":
     test_number_of_ways()
